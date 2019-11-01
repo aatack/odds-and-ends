fd635f4a2d8cf868fb99ccc71ccf3e7b9d107d27
aatack <alexanderjtatack@gmail.com>
2019-11-01 18:55:42 +0000

    Add simple entry creation

--- learn-lisp/a-simple-database.lisp
+++ learn-lisp/a-simple-database.lisp
@@ -8,3 +8,19 @@
 (defun dump-db ()
     (dolist (cd *db*)
     (format t "~{~a:~10t~a~%~}~%" cd)))
+
+(defun prompt-read (prompt)
+    (format *query-io* "~a: " prompt)
+    (force-output *query-io*)
+    (read-line *query-io*))
+
+(defun prompt-for-cd ()
+    (make-cd
+        (prompt-read "Title")
+        (prompt-read "Artist")
+        (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+        (y-or-n-p "Ripped")))
+
+(dump-db)
+(add-record (prompt-for-cd))
+(dump-db)
