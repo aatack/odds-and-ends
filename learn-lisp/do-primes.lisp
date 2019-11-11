4f6bee6b424869ebfcc01877294ed7f2522363ff
aatack <alexanderjtatack@gmail.com>
2019-11-11 21:08:40 +0000

    Define utility functions

---
+++ learn-lisp/do-primes.lisp
@@ -0,0 +1,6 @@
+(defun prime-p (number)
+  (when (> number 1))
+    (loop for fac from 2 to (isqrt number) never (zerop (mod number fac))))
+
+(defun next-prime (number)
+  (loop for n from number when (prime-p n) return n))
