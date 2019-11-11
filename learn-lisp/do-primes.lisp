f710c6de6c8f3179c2b88db31d1231954abcf74b
aatack <alexanderjtatack@gmail.com>
2019-11-11 21:43:43 +0000

    Nest macros for fixing leaks

--- learn-lisp/do-primes.lisp
+++ learn-lisp/do-primes.lisp
@@ -6,8 +6,12 @@
   (loop for n from number when (prime-p n) return n))
 
 (defmacro do-primes ((var start end) &body body)
-  (let ((ending-value-name (gensym)))
+  (with-gensyms-1 (ending-value-name)
     `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
           (,ending-value-name ,end))
         ((> ,var ,ending-value-name))
       ,@body)))
+
+(defmacro with-gensyms-1 ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+      ,@body))
