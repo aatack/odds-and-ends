241196ccb8098d50bd3c133682a92f943b448e5b
aatack <alexanderjtatack@gmail.com>
2019-11-26 01:12:38 +0000

    Add docstrings to each macro

--- learn-lisp/unit-testing.lisp
+++ learn-lisp/unit-testing.lisp
@@ -1,8 +1,24 @@
 (defun report-result (form)
+  "Check that a form evaluates to t, printing a helpful readout."
   (let ((result (eval form)))
     (format t "~%~:[FAIL~;Pass~] --- ~a" result form)
     result))
 
 (defmacro check (&rest forms)
+  "Evaluate a list of forms and return their results."
   `(progn
      ,@(loop for f in forms collect `(report-result ',f))))
+
+(defmacro use-gensyms (names &body body)
+  "Assign a gensym to each symbol in name, then execute the body."
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro combine-results (&body forms)
+  "Return t iff all forms evaluate to t."
+  (use-gensyms (result)
+    `(let ((,result t))
+      ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+      ,result)))
+
+(defvar test-forms '((= 1 2) (= 2 2) (= 3 2)))
