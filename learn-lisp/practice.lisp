c89b424f23baf4911501ff4a2b83318c3e96e4e1
aatack <alexanderjtatack@gmail.com>
2020-02-14 16:31:29 +0000

    Implement presentp function

--- learn-lisp/practice.lisp
+++ learn-lisp/practice.lisp
@@ -1,2 +1,9 @@
 (defun palindromep (input)
   (eval `(and ,@(mapcar (lambda (a b) (eql a b)) input (reverse input)))))
+
+(defun presentp (target input)
+  (if input
+    (if (eq target (car input))
+      t
+      (presentp target (cdr input)))
+    nil))
