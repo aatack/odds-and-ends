4f7a204538b6bb36b408e52c3da7a64b5255ade3
aatack <alexanderjtatack@gmail.com>
2020-02-14 16:42:00 +0000

    Allow presentp to work on trees

--- learn-lisp/practice.lisp
+++ learn-lisp/practice.lisp
@@ -1,9 +1,9 @@
 (defun palindromep (input)
+  "Determine whether or not a list is a palindrome."
   (eval `(and ,@(mapcar (lambda (a b) (eql a b)) input (reverse input)))))
 
 (defun presentp (target input)
-  (if input
-    (if (eq target (car input))
-      t
-      (presentp target (cdr input)))
-    nil))
+  "Determine whether an atom appears anywhere in a tree."
+  (if (or (null input) (atom input))
+    (eql target input)
+    (eval `(or ,@(mapcar (lambda (child) (presentp target child)) input)))))
