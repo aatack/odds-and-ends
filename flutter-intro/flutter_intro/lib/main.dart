f9534fe8ba2613dc5a12fc92e55f64e41fe569c0
aatack <alexanderjtatack@gmail.com>
2023-04-03 00:11:11 +0100

    Do some further refactoring

--- flutter-intro/flutter_intro/lib/main.dart
+++ flutter-intro/flutter_intro/lib/main.dart
@@ -42,16 +42,19 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea to work with:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text("Next"))
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text("Next"))
+          ],
+        ),
       ),
     );
   }
@@ -79,7 +82,11 @@ class BigCard extends StatelessWidget {
         color: theme.colorScheme.primary,
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Text(pair.asLowerCase, style: style),
+          child: Text(
+            pair.asLowerCase,
+            style: style,
+            semanticsLabel: "${pair.first} ${pair.second}",
+          ),
         ),
       ),
     );
