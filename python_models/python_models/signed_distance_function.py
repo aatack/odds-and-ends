c06645beb719c496c6e8adbbad2b4ee761576dd4
aatack <alexanderjtatack@gmail.com>
2025-05-14 01:51:03 +0100

    Automatically infer SDF bounds

--- python_models/python_models/signed_distance_function.py
+++ python_models/python_models/signed_distance_function.py
@@ -10,14 +10,33 @@ class SignedDistanceFunction:
         self.function = function
 
     def mesh(
-        self, x: int | None = None, y: int | None = None, z: int | None = None
+        self,
+        x: int | None = None,
+        y: int | None = None,
+        z: int | None = None,
+        distance: float = 1e5,
+        buffer: float = 0.1,
     ) -> pv.MultiBlock:
         x = x or y or z or 50
         y = y or x or z or 50
         z = z or x or y or 50
 
         xs, ys, zs = np.meshgrid(
-            np.linspace(-1, 1, x), np.linspace(-1, 1, y), np.linspace(-1, 1, z)
+            np.linspace(
+                -distance + self.function(-distance, 0, 0) - buffer,
+                distance - self.function(distance, 0, 0) + buffer,
+                x,
+            ),
+            np.linspace(
+                -distance + self.function(0, -distance, 0) - buffer,
+                distance - self.function(0, distance, 0) + buffer,
+                y,
+            ),
+            np.linspace(
+                -distance + self.function(0, 0, -distance) - buffer,
+                distance - self.function(0, 0, distance) + buffer,
+                z,
+            ),
         )
 
         coordinates = np.vstack([xs.ravel(), ys.ravel(), zs.ravel()]).T
@@ -30,7 +49,9 @@ class SignedDistanceFunction:
             signature="(n)->()",
         )(coordinates)
 
-        return grid.contour([0.0], scalars="sdf")
+        contours = grid.contour([0.0], scalars="sdf")
+        del contours.point_data["sdf"]
+        return contours
 
     def grow(self, distance: float) -> "SignedDistanceFunction":
         return SignedDistanceFunction(lambda x, y, z: self.function(x, y, z) - distance)
