e6414f57247c19eaa8d4de736a56a96407a43418
aatack <alexanderjtatack@gmail.com>
2025-08-01 20:42:13 +0100

    Bundle the outputs up into a handy object

--- adversarial_image_classification/adversarial_image_classification/noise_addition.py
+++ adversarial_image_classification/adversarial_image_classification/noise_addition.py
@@ -1,3 +1,4 @@
+from typing import NamedTuple
 import torch
 from adversarial_image_classification.class_names import get_class_names
 from adversarial_image_classification.model import build_model
@@ -5,11 +6,30 @@ from torch.optim import Adam
 from tqdm import tqdm
 
 
+class AdversarialNoise(NamedTuple):
+    target_class: str
+
+    input_image: torch.Tensor
+    output_image: torch.Tensor
+
+    @property
+    def noise(self) -> torch.Tensor:
+        return self.output_image - self.input_image
+
+    @property
+    def initial_probability(self) -> float:
+        return get_class_probability(self.input_image, self.target_class).item()
+
+    @property
+    def final_probability(self) -> float:
+        return get_class_probability(self.output_image, self.target_class).item()
+
+
 def get_class_probability(image: torch.Tensor, class_name: str) -> torch.Tensor:
     return build_model()(image)[get_class_names().index(class_name)]
 
 
-def maximise_probability(image: torch.Tensor, class_name: str) -> torch.Tensor:
+def maximise_probability(image: torch.Tensor, class_name: str) -> AdversarialNoise:
     parameter = torch.nn.parameter.Parameter(image.clone())
     optimiser = Adam(params=[parameter], lr=3e-4)
     criterion = torch.nn.BCELoss()
@@ -18,8 +38,7 @@ def maximise_probability(image: torch.Tensor, class_name: str) -> torch.Tensor:
         optimiser.zero_grad()
         probability = get_class_probability(parameter, class_name)
         loss = criterion(probability, torch.tensor(1.0))
-        print(probability.item())
         loss.backward()
         optimiser.step()
 
-    return parameter
+    return AdversarialNoise(class_name, image, parameter.data)
