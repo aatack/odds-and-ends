1e80a3915de916e74a7828aeb1a17f1bde6c2b02
aatack <alexanderjtatack@gmail.com>
2025-08-01 20:34:59 +0100

    Add a helper for rendering the image

--- adversarial_image_classification/adversarial_image_classification/helpers.py
+++ adversarial_image_classification/adversarial_image_classification/helpers.py
@@ -1,8 +1,17 @@
 import torch
 from torchvision.transforms.functional import pil_to_tensor
 import pyperclipimg
+from torchvision.transforms import ToPILImage
+from PIL.Image import Image
 
 
 def paste_image() -> torch.Tensor:
     """Paste an image, in 0-1 (3, W, H) format, as a torch tensor from the clipboard."""
     return pil_to_tensor(pyperclipimg.paste()) / 256
+
+
+def render_tensor(image: torch.Tensor, normalise: bool = False) -> Image:
+    if normalise:
+        image -= image.min()
+        image /= image.max()
+    return ToPILImage()(image)
