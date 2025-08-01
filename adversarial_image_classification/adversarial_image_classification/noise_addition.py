bdc7f1398d29b11171e76efdf694ed4714c1c7c0
aatack <alexanderjtatack@gmail.com>
2025-08-01 20:30:58 +0100

    Maximise the probability over a few steps

---
+++ adversarial_image_classification/adversarial_image_classification/noise_addition.py
@@ -0,0 +1,25 @@
+import torch
+from adversarial_image_classification.class_names import get_class_names
+from adversarial_image_classification.model import build_model
+from torch.optim import Adam
+from tqdm import tqdm
+
+
+def get_class_probability(image: torch.Tensor, class_name: str) -> torch.Tensor:
+    return build_model()(image)[get_class_names().index(class_name)]
+
+
+def maximise_probability(image: torch.Tensor, class_name: str) -> torch.Tensor:
+    parameter = torch.nn.parameter.Parameter(image)
+    optimiser = Adam(params=[parameter], lr=3e-4)
+    criterion = torch.nn.BCELoss()
+
+    for _ in tqdm(range(10)):
+        optimiser.zero_grad()
+        probability = get_class_probability(parameter, class_name)
+        loss = criterion(probability, torch.tensor(1.0))
+        print(probability.item())
+        loss.backward()
+        optimiser.step()
+
+    return parameter
