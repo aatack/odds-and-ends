9f5bed4cce36de4a4b9c2d66c2fb1647e7db765f
aatack <alexanderjtatack@gmail.com>
2025-08-13 02:48:27 +0100

    Output durations per user

--- log_files/log_files/models.py
+++ log_files/log_files/models.py
@@ -1,7 +1,29 @@
+from datetime import date
 from pydantic import BaseModel
 
 
-class DurationStatistics(BaseModel):
+class MethodDurations(BaseModel):
+    method_name: str
+
     mean: float
     median: float
     mode: float
+
+
+class UserActivity(BaseModel):
+    user_id: str
+    days_active: list[date]
+
+
+class UserDurations(BaseModel):
+    user_id: str
+
+    total_time_seconds: float
+    mean_time_seconds: float
+
+
+class UserDurationsData(BaseModel):
+    users: list[UserDurations]
+
+    highest: list[UserDurations]
+    lowest: list[UserDurations]
--- log_files/log_files/statistics.py
+++ log_files/log_files/statistics.py
@@ -1,28 +1,60 @@
 from collections import defaultdict
 from datetime import date
 from log_files.logs import Log
-from log_files.models import DurationStatistics
 import statistics
 
+from log_files.models import (
+    MethodDurations,
+    UserActivity,
+    UserDurations,
+    UserDurationsData,
+)
 
-def method_duration_statistics(logs: list[Log]) -> dict[str, DurationStatistics]:
+
+def method_durations(logs: list[Log]) -> list[MethodDurations]:
     method_durations: dict[str, list[float]] = defaultdict(list)
     for log in logs:
         method_durations[log.method_name].append(log.duration_seconds)
 
-    return {
-        method_name: DurationStatistics(
+    return [
+        MethodDurations(
+            method_name=method_name,
             mean=statistics.mean(durations),
             median=statistics.median(durations),
             mode=statistics.mode(durations),
         )
         for method_name, durations in method_durations.items()
-    }
+    ]
 
 
-def user_activity(logs: list[Log]) -> dict[str, list[date]]:
+def user_activity(logs: list[Log]) -> list[UserActivity]:
     user_days: dict[str, set[date]] = defaultdict(set)
     for log in logs:
         user_days[log.user_id].add(log.timestamp.date())
 
-    return {user: sorted(days) for user, days in user_days.items()}
+    return [
+        UserActivity(user_id=user, days_active=sorted(days))
+        for user, days in user_days.items()
+    ]
+
+
+def user_durations(logs: list[Log]) -> UserDurationsData:
+    user_logs: dict[str, list[float]] = defaultdict(list)
+    for log in logs:
+        user_logs[log.user_id].append(log.duration_seconds)
+
+    durations = sorted(
+        [
+            UserDurations(
+                user_id=user,
+                total_time_seconds=sum(durations),
+                mean_time_seconds=statistics.mean(durations),
+            )
+            for user, durations in user_logs.items()
+        ],
+        key=lambda user: user.total_time_seconds,
+    )
+
+    return UserDurationsData(
+        users=durations, highest=durations[-5:][::-1], lowest=durations[:5]
+    )
