186505da538df3d2f36fc3df5af14a7e829a913f
aatack <alexanderjtatack@gmail.com>
2025-08-15 16:03:12 +0100

    Implement the second solution

--- engineering/engineering/cooc.py
+++ engineering/engineering/cooc.py
@@ -5,6 +5,10 @@
 # c(alice, dan) == 0
 
 
+from collections import defaultdict
+import itertools
+
+
 class Cooc:
     def __init__(self):
         self.lines: list[set[str]] = []
@@ -14,3 +18,17 @@ class Cooc:
 
     def get(self, a: str, b: str) -> int:
         return len([line for line in self.lines if (a in line) and (b in line)])
+
+
+class Cooc2:
+    def __init__(self):
+        self.index: dict[tuple[str, str], int] = defaultdict(lambda: 0)
+
+    def update(self, line: str):
+        unique_strings = set(line.split())
+        for a, b in itertools.product(unique_strings, unique_strings):
+            if a < b:
+                self.index[tuple(sorted([a, b]))] += 1
+
+    def get(self, a: str, b: str) -> int:
+        return self.index[a, b]
