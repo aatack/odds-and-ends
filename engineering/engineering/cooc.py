2ef30d91ae814002c79e5fdedcfd3594ab598c49
aatack <alexanderjtatack@gmail.com>
2025-08-20 22:53:34 +0100

    Tidy up the implementation

--- engineering/engineering/cooc.py
+++ engineering/engineering/cooc.py
@@ -1,10 +1,3 @@
-# alice bob clair
-# bob clair dan
-
-# c(alice, bob) == 1
-# c(alice, dan) == 0
-
-
 from collections import defaultdict
 import itertools
 
@@ -20,15 +13,14 @@ class Cooc:
         return len([line for line in self.lines if (a in line) and (b in line)])
 
 
-class Cooc2:
+class CachedCooc:
     def __init__(self):
         self.index: dict[tuple[str, str], int] = defaultdict(lambda: 0)
 
     def update(self, line: str):
         unique_strings = set(line.split())
-        for a, b in itertools.product(unique_strings, unique_strings):
-            if a < b:
-                self.index[tuple(sorted([a, b]))] += 1
+        for a, b in itertools.combinations(unique_strings, unique_strings):
+            self.index[tuple(sorted([a, b]))] += 1
 
     def get(self, a: str, b: str) -> int:
         return self.index[a, b]
