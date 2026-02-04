33e75f0fb9a9897db23e1bdba701100df45052f1
aatack <alexanderjtatack@gmail.com>
2026-02-04 01:16:32 +0000

    Render a simple canvas

--- rendering/rendering-app/src/App.tsx
+++ rendering/rendering-app/src/App.tsx
@@ -1,10 +1,19 @@
+import { Canvas } from "@react-three/fiber";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div style={{ height: "100%" }}>Canvas</div>
-    </>
+    <div style={{ width: "100%", height: "100%", background: "#111" }}>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      </Canvas>
+    </div>
   );
 }
 
