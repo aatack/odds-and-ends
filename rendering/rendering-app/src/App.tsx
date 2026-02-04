374839e70ec142002c581621386257a99d154517
aatack <alexanderjtatack@gmail.com>
2026-02-04 01:23:27 +0000

    Enable orbit controls

--- rendering/rendering-app/src/App.tsx
+++ rendering/rendering-app/src/App.tsx
@@ -1,16 +1,27 @@
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { useState } from "react";
 
 function App() {
+  const [hovered, setHovered] = useState(false);
+
   return (
-    <div style={{ width: "100%", height: "100%", background: "#111" }}>
+    <div style={{ width: "100%", height: "100%", background: "#222" }}>
       <Canvas>
         <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
+        <PerspectiveCamera makeDefault position={[5, 3, 4]}>
+          <directionalLight position={[0, 0, 2]} intensity={0.5} />
+        </PerspectiveCamera>
 
-        <mesh>
+        <OrbitControls makeDefault enableDamping={false} />
+
+        <mesh
+          onPointerEnter={() => setHovered(true)}
+          onPointerLeave={() => setHovered(false)}
+        >
           <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="orange" />
+          <meshStandardMaterial color={hovered ? "orange" : "lightblue"} />
         </mesh>
       </Canvas>
     </div>
